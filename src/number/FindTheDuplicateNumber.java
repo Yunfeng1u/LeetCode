c3248a88492fa17a6d939395136669c4cdc4174f
卢运烽 <414675087@qq.com>
2018-01-25 22:54:26 +0800

    287. Find the Duplicate Number

---
+++ src/number/FindTheDuplicateNumber.java
@@ -0,0 +1,45 @@
+package number;
+
+import utils.Log;
+
+/**
+ * 287. Find the Duplicate Number
+ * <p>
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ * <p>
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+ * prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+ * <p>
+ * Note:
+ * You must not modify the array (assume the array is read only).
+ * You must use only constant, O(1) extra space.
+ * Your runtime complexity should be less than O(n2).
+ * There is only one duplicate number in the array, but it could be repeated more than once.
+ */
+public class FindTheDuplicateNumber {
+    public static void main(String[] args) {
+        int result = findDuplicate(new int[]{1,4,6,6,6,2,3});
+        Log.d(result);
+    }
+
+    public static int findDuplicate(int[] nums) {
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+}
