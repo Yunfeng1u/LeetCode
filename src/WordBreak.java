f1e64161545ee2cc414424c9d3dff9c5777b77e2
卢运烽 <414675087@qq.com>
2018-11-04 08:51:58 +0800

    139. Word Break

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * todo 139. Word Break
+ * 139. Word Break
  * https://leetcode.com/problems/word-break/
  * <p>
  * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
@@ -29,10 +29,10 @@ import java.util.List;
  */
 public class WordBreak {
     public static void main(String[] args) {
-//        System.out.println(wordBreak("applepenapple", new ArrayList<String>() {{
-//            add("apple");
-//            add("pen");
-//        }}));
+        System.out.println(wordBreak("applepenapple", new ArrayList<String>() {{
+            add("apple");
+            add("pen");
+        }}));
         System.out.println(wordBreak("catsandog", new ArrayList<String>() {{
             add("cats");
             add("dog");
@@ -43,40 +43,24 @@ public class WordBreak {
     }
 
     public static boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] f = new boolean[s.length() + 1];
 
-        f[0] = true;
+        boolean[] dp = new boolean[s.length() + 1];
 
+        dp[0] = true;
 
-        for(int i = 1; i <= s.length(); i++){
-            // 循环每个字典词
-            for(String str: wordDict){
-                if(str.length() <= i){
-                    // 只有前面的index为止的单词能从字典里找到，下一个才有可能符合
-                    if(f[i - str.length()]){
-                        // 截取以index为止，与字典词长度相等的字符串，与字典词比较
-                        if(s.substring(i-str.length(), i).equals(str)){
-                            f[i] = true;
-                            break;
+        for (int i = 1; i <= s.length(); i++) {
+            for (String word : wordDict) {
+                if (word.length() <= i) {
+                    if (dp[i - word.length()]) {
+                        String toBeCompared = s.substring(i - word.length(), i);
+                        if (word.equals(toBeCompared)) {
+                            dp[i] = true;
                         }
                     }
                 }
             }
         }
-
-        //Second DP
-        /*
-        for(int i=1; i <= s.length(); i++){
-            for(int j=0; j < i; j++){
-                if(f[j] && wordDict.contains(s.substring(j, i))){
-                    f[i] = true;
-                    break;
-                }
-            }
-        }
-        */
-
-        return f[s.length()];
+        return dp[s.length()];
     }
 
 }
