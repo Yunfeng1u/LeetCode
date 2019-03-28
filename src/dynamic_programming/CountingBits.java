7ee0e13a2271fb24bdedd29d6564298f1adb8334
卢运烽 <414675087@qq.com>
2019-03-28 17:54:10 +0800

    338. Counting Bits

--- src/dynamic_programming/CountingBits.java
+++ src/dynamic_programming/CountingBits.java
@@ -1,10 +1,6 @@
 package dynamic_programming;
 
-import sun.jvm.hotspot.utilities.Assert;
 import utils.Log;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 338. Counting Bits
@@ -23,8 +19,12 @@ public class CountingBits {
     }
 
     public static int[] countBits(int num) {
+        // counts[i]代表中i含有几个1
         int[] counts = new int[num + 1];
         for (int i = 1; i <= num; i++) {
+            // i >> 1, 右移一位，掐去最后一位
+            // counts[i >> 1]代表i掐去最后最后一位有几个1，计算结果肯定前边已经得出（理解为除以2）
+            // 再看最后一位是不是1。&操作符: 只有同为1结果才为1
             counts[i] = counts[i >> 1] + (i & 1);
         }
         return counts;
